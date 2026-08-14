04a8ea4e0b3c75e4f56f8070bdb55890e09406fc
Shailesh-1302 <subham.ptgh@gmail.com>
2026-08-15 00:48:45 +0530

    problem:To Check Armstrong number

--- prac/armstrongnum.cpp
+++ prac/armstrongnum.cpp
@@ -1,11 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 bool isArmstrong(int n){
-    
+    int k=to_string(n).length();
+    int sum=0;
+    int cpy=n;
+    while(n>0){
+        int id=n%10;
+        sum=sum+pow(id,k);
+        n=n/10;
+    }
+    return cpy==sum ;// returns true if equals
 }
 int main(){
-    int n=4;
-    cout<<isArmstrong;
+    int n=9474;
+
+   if(isArmstrong(n)){
+    cout<<n<<" is an armstrong number"<<endl;
+   }else{
+    cout<<n<<" is not an armstrong number"<<endl;
+   }
 
     return 0;
 }
